3a8952e17871234e721e3a5a911f960600f3cedb
NicholasWjy24 <nicholas.wjy24@gmail.com>
2025-12-24 14:22:36 +0700

    feat: penambahan textfield untuk password dan confirm password beserta logic visible

--- lib/view/Register_Screen.dart
+++ lib/view/Register_Screen.dart
@@ -15,25 +15,78 @@ final authService = AuthService();
 class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController birthDateController = TextEditingController();
   final TextEditingController genderController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
 
+  bool obscureTextPassword = true;
+  bool obscureTextConfirmPassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsetsGeometry.all(8),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text('Register'),
               TextField(
-
-              )
+                controller: usernameController,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  labelText: "Username",
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: passwordController,
+                obscureText: obscureTextPassword,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      obscureTextPassword = !obscureTextPassword;
+                      setState(() {
+                        obscureTextPassword;
+                      });
+                    },
+                    icon: obscureTextPassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextField(
+                controller: confirmPasswordController,
+                obscureText: obscureTextConfirmPassword,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      obscureTextConfirmPassword = !obscureTextConfirmPassword;
+                      setState(() {
+                        obscureTextConfirmPassword;
+                      });
+                    },
+                    icon: obscureTextConfirmPassword ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  labelText: "Re-type Password",
+                ),
+              ),
             ],
           ),
         ),
